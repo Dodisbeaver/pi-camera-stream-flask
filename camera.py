b0ed10cb5888640282de393e1c4a43b80583b662
Ubuntu <ubuntu@ubuntu.tailba481.ts.net>
2024-04-02 08:58:18 +0000

    use opencv instead of picam

--- camera.py
+++ camera.py
@@ -3,23 +3,27 @@
 #Desc: This scrtipt script..
 
 import cv2 as cv
-from imutils.video.pivideostream import PiVideoStream
+# from imutils.video.pivideostream import PiVideoStream
 import imutils
 import time
 from datetime import datetime
 import numpy as np
 
 class VideoCamera(object):
-    def __init__(self, flip = False, file_type  = ".jpg", photo_string= "stream_photo"):
+    def __init__(self, flip = False, file_type  = ".jpg", photo_string= "stream_photo", camera_index=0):
         # self.vs = PiVideoStream(resolution=(1920, 1080), framerate=30).start()
-        self.vs = PiVideoStream().start()
+        self.vs = cv.VideoCapture(camera_index)
         self.flip = flip # Flip frame vertically
         self.file_type = file_type # image type i.e. .jpg
         self.photo_string = photo_string # Name to save the photo
         time.sleep(2.0)
+        
+        if not self.vs.isOpened():
+            raise ValueError("Unable to open USB camera")
+
 
     def __del__(self):
-        self.vs.stop()
+        self.vs.release()
 
     def flip_if_needed(self, frame):
         if self.flip:
@@ -27,14 +31,20 @@ class VideoCamera(object):
         return frame
 
     def get_frame(self):
-        frame = self.flip_if_needed(self.vs.read())
+
+        ret, frame = self.flip_if_needed(self.vs.read())
+        if not ret:  # Check if frame was read successfully
+            print("Error reading frame, check camera connection")
+            return  # Or raise an exception if needed
+ 
+    
         ret, jpeg = cv.imencode(self.file_type, frame)
         self.previous_frame = jpeg
         return jpeg.tobytes()
 
     # Take a photo, called by camera button
     def take_picture(self):
-        frame = self.flip_if_needed(self.vs.read())
+        ret, frame = self.flip_if_needed(self.vs.read())
         ret, image = cv.imencode(self.file_type, frame)
         today_date = datetime.now().strftime("%m%d%Y-%H%M%S") # get current time
         cv.imwrite(str(self.photo_string + "_" + today_date + self.file_type), frame)
