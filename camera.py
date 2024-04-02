df1c8e0f913b0d42f37fcf65606556006b7b8236
dodisbeaver <dodisbeaver@gmail.com>
2024-04-02 11:09:00 +0000

    draft of object detection

--- camera.py
+++ camera.py
@@ -2,6 +2,26 @@
 #Date: 27.09.20
 #Desc: This scrtipt script..
 
+classNames = {0: 'background',
+              1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
+              7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
+              13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
+              18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
+              24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
+              32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
+              37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
+              41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
+              46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
+              51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
+              56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
+              61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
+              67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
+              75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
+              80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
+              86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
+
+
+
 import cv2 as cv
 # from imutils.video.pivideostream import PiVideoStream
 import time
@@ -15,6 +35,9 @@ class VideoCamera(object):
         self.flip = flip # Flip frame vertically
         self.file_type = file_type # image type i.e. .jpg
         self.photo_string = photo_string # Name to save the photo
+        self.model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb',
+                                      'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
+
         time.sleep(2.0)
         
         if not self.vs.isOpened():
@@ -35,8 +58,24 @@ class VideoCamera(object):
         if not ret:  # Check if frame was read successfully
             print("Error reading frame, check camera connection")
             return  # Or raise an exception if needed
- 
-    
+        image_height, image_width, _ = frame.shape
+        self.model.setInput(cv2.dnn.blobFromImage(frame, size=(300,300), swapRB=True))
+        output = model.forward()
+
+        for detection in output[0, 0, :, :]:
+            confidence = detection[2]
+            if confidence > .5:
+                class_id = detection[1]
+                class_name=id_class_name(class_id,classNames)
+                print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
+                box_x = detection[3] * image_width
+                box_y = detection[4] * image_height
+                box_width = detection[5] * image_width
+                box_height = detection[6] * image_height
+                cv2.rectangle(frame, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
+                cv2.putText(frame,class_name ,(int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(.005*image_width),(0, 0, 255))
+
+        
         ret, jpeg = cv.imencode(self.file_type, frame)
         self.previous_frame = jpeg
         return jpeg.tobytes()
@@ -47,3 +86,9 @@ class VideoCamera(object):
         ret, image = cv.imencode(self.file_type, frame)
         today_date = datetime.now().strftime("%m%d%Y-%H%M%S") # get current time
         cv.imwrite(str(self.photo_string + "_" + today_date + self.file_type), frame)
+
+
+    def id_class_name(class_id, classes):
+        for key, value in classes.items():
+            if class_id == key:
+                return value
