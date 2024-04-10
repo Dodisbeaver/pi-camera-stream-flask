5c6fae037aa51cf25b8547fcc7e407ce8d249de3
dodis <dodisbeaver@gmail.com>
2024-04-10 15:28:29 +0300

    edge-tpu

--- camera.py
+++ camera.py
@@ -27,7 +27,7 @@ class VideoCamera(object):
         self.photo_string = photo_string # Name to save the photo
         self.model = cv.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb',
                                       'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
-        self.interpreter = make_interpreter('models/mobilenet_v2_1.0_224_quant_edgetpu.tflite')
+        self.interpreter = make_interpreter('models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite')
         self.interpreter.allocate_tensors()
         self.labels = read_label_file('models/coco_labels.txt')
         self.inference_size = input_size(self.interpreter)
@@ -100,10 +100,11 @@ class VideoCamera(object):
 
         cv2_im_rgb = cv.cvtColor(cv2_im, cv.COLOR_BGR2RGB)
         cv2_im_rgb = cv.resize(cv2_im_rgb, self.inference_size)
+        print('trying to run inference')
         run_inference(self.interpreter, cv2_im_rgb.tobytes())
         print('we are running inference')
         objs = get_objects(self.interpreter, 0.5)[:3]
-        cv2_im = append_objs_to_img(cv2_im, self.inference_size, objs, labels)
+        cv2_im = append_objs_to_img(cv2_im, self.inference_size, objs, self.labels)
 
         
         ret, jpeg = cv.imencode('.jpg', cv2_im)
@@ -114,13 +115,6 @@ class VideoCamera(object):
         self.previous_frame = jpeg
         return jpeg.tobytes()
 
-    # Take a photo, called by camera button
-    def take_picture(self):
-        ret, frame = self.flip_if_needed(self.vs.read())
-        ret, image = cv.imencode(self.file_type, frame)
-        today_date = datetime.now().strftime("%m%d%Y-%H%M%S") # get current time
-        cv.imwrite(str(self.photo_string + "_" + today_date + self.file_type), frame)
-    
     def append_objs_to_img(cv2_im, inference_size, objs, labels):
         height, width, channels = cv2_im.shape
         scale_x, scale_y = width / inference_size[0], height / inference_size[1]
@@ -132,10 +126,19 @@ class VideoCamera(object):
             percent = int(100 * obj.score)
             label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
 
-            cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-            cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+            cv2_im = cv.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+            cv2_im = cv.putText(cv2_im, label, (x0, y0+30),
+                                cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
         return cv2_im
+
+    # Take a photo, called by camera button
+    def take_picture(self):
+        ret, frame = self.flip_if_needed(self.vs.read())
+        ret, image = cv.imencode(self.file_type, frame)
+        today_date = datetime.now().strftime("%m%d%Y-%H%M%S") # get current time
+        cv.imwrite(str(self.photo_string + "_" + today_date + self.file_type), frame)
+    
+    
 
     def release():
         vs.release()
